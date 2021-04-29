c0b3c77e9171f265f03fcc2045efeaf1de318ada
unknown <grisha1996111616@gmail.com>
2021-04-29 14:54:05 +0300

    add translate on english

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import './styles.css';
-import './java-script/switch.js'
-import objectData from './menu.json'
-import menuTemplate from './templates/galery.hbs';
+import './java-script/switch.js';
+import objectData from './menu.json';
+import menuTemplate from './templates/gallery.hbs';
 const markup = menuTemplate(objectData);
-const redyRenderingUl = document.querySelector('.js-menu');
-redyRenderingUl.insertAdjacentHTML('afterbegin', markup);
-
-
-
-
-
-
+const readyRenderingUl = document.querySelector('.js-menu');
+readyRenderingUl.insertAdjacentHTML('afterbegin', markup);
--- src/java-script/switch.js
+++ src/java-script/switch.js
@@ -1,47 +1,42 @@
-export 
-const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
-    const switchThem = document.querySelector('.switch');
-    const changeHueForBody = document.querySelector('body');
-    const changeValueForInput = document.querySelector('.js-switch-input');
-    window.addEventListener('DOMContentLoaded',ev => {
-        const savedSettings = localStorage.getItem('them');
-        const parsedSettings = JSON.parse(savedSettings); 
-            if(parsedSettings === Theme.LIGHT){
-                changeHueForBody.classList.add(Theme.LIGHT);
-                changeValueForInput.checked = false;
-                console.log(`change them on LIGHT from local storage`);
-            } else if(parsedSettings === Theme.DARK){
-            changeHueForBody.classList.add(Theme.DARK);
-            changeValueForInput.checked = true;
-            console.log(`change them on DARK from local storage`);
-            } else {
-            console.log(`base sethings for Page when you are visit in the first is THEM LIGHT when we are opening in the first`)
-            changeHueForBody.classList.add(Theme.LIGHT);
-            changeValueForInput.checked = false;
-        }
-    });
+export const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const switchThem = document.querySelector('.switch');
+const changeHueForBody = document.querySelector('body');
+const changeValueForInput = document.querySelector('.js-switch-input');
+window.addEventListener('DOMContentLoaded', ev => {
+  const savedSettings = localStorage.getItem('them');
+  const parsedSettings = JSON.parse(savedSettings);
+  if (parsedSettings === Theme.LIGHT) {
+    changeHueForBody.classList.add(Theme.LIGHT);
+    changeValueForInput.checked = false;
+  } else if (parsedSettings === Theme.DARK) {
+    changeHueForBody.classList.add(Theme.DARK);
+    changeValueForInput.checked = true;
+  } else {
+    changeHueForBody.classList.add(Theme.LIGHT);
+    changeValueForInput.checked = false;
+  }
+});
 
-    switchThem.addEventListener('change',switchThemEvent);
-    function switchThemEvent(){
-        if(changeHueForBody.classList.contains(Theme.LIGHT) ){
-            changeHueForBody.classList.replace(Theme.LIGHT,Theme.DARK);
-            localStorage.setItem('them', JSON.stringify(Theme.DARK));
-            return;
-        }
-        if(changeHueForBody.classList.contains(Theme.DARK)){
-            changeHueForBody.classList.replace(Theme.DARK,Theme.LIGHT);
-            localStorage.setItem('them', JSON.stringify(Theme.LIGHT));
-            return;
-        }
-    };
-// Реализуй функционал изменения темы при нажатии (событие change) на чекбокс input.js-switch-input в тулбаре.
-// По умолчанию тема светлая.
-// При изменении темы, необходимо добавлять на элемент body класс light-theme или dark-theme.
-// Выбранная тема должна сохраняться между перезагрузками страницы. Для хранения активной темы используй localStorage.
-// Если при загрузке страницы тема темная, не забудь поставить свойство checked у чекбокса input.js-switch-input в true,
-//  чтобы ползунок сдвинулся в правильное положение.
-// Для удобства хранения списка тем используй такое перечисление Theme.
-
+switchThem.addEventListener('change', switchThemEvent);
+function switchThemEvent() {
+  if (changeHueForBody.classList.contains(Theme.LIGHT)) {
+    changeHueForBody.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('them', JSON.stringify(Theme.DARK));
+    return;
+  }
+  if (changeHueForBody.classList.contains(Theme.DARK)) {
+    changeHueForBody.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('them', JSON.stringify(Theme.LIGHT));
+    return;
+  }
+}
+// Realize functional for switch them during (event "change") on checkbox input.js-switch-input in toolbar .
+// Base them is light.
+// During them change are necessary on element "body" class "light-them" or "dark-them" .
+// Chosen them has to save between page reload .For save active them use localStorage .
+// If them is dark during download page , don't forgot property "checked" in checkbox input.js-switch-input on "true".
+// For switcher move to right position.
+// For comfortable save them list use like this naming "Them".
